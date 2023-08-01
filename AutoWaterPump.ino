88ca9324a68ee1de48d2c1be844608a32cebbaf4
Deiraheler <53556704+Deiraheler@users.noreply.github.com>
2023-08-01 21:33:05 +0100

    FullCode AutoWaterPump.ino

--- AutoWaterPump.ino
+++ AutoWaterPump.ino
@@ -1,21 +1,32 @@
-int relayPin = 2; // Change this to the pin you used
-// define the sensor pin
-const int sensorPin = A0;
+// Set up your input and output pins
+int moistureSensor = A0;
+int relay = 2;
+int sensorValue = 0;  
+
+// Define the dryness threshold. You might need to adjust this value based on your specific sensor and soil conditions
+int dryThreshold = 300;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(relayPin, OUTPUT);
+  // Set up the relay pin as an output
+  pinMode(relay, OUTPUT);
 }
 
 void loop() {
-  int sensorValue = analogRead(sensorPin);
+  // Read the value from the moisture sensor
+  sensorValue = analogRead(moistureSensor);
 
-  // print out the value to the serial monitor
-  Serial.println(sensorValue);
+  // Map the sensor value to a percentage (assuming 500 is 100% and 0 is 0%)
+  int moisturePercent = map(sensorValue, 0, 550, 0, 100);
 
-  // delay between reads
+  // If the sensor value is below the dryness threshold, activate the pump
+  if (sensorValue < dryThreshold) {
+    digitalWrite(relay, HIGH);
+  } 
+  // If the sensor value is above the dryness threshold, deactivate the pump
+  else {
+    digitalWrite(relay, LOW);
+  }
+
+  // Wait for a bit before checking again
   delay(1000);
-  digitalWrite(relayPin, HIGH); // turns on the relay and the pump
-  delay(2000);
-  digitalWrite(relayPin, LOW); // turns on the relay and the pump
 }

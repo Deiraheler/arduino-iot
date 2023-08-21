d3f93fff080db84e36730d02469a19400a64306e
Deiraheler <53556704+Deiraheler@users.noreply.github.com>
2023-08-21 20:53:23 +0100

    Update AutoWaterPump.ino

--- AutoWaterPump.ino
+++ AutoWaterPump.ino
@@ -2,9 +2,7 @@
 int moistureSensor = A0;
 int relay = 2;
 int sensorValue = 0;  
-
-// Define the dryness threshold. You might need to adjust this value based on your specific sensor and soil conditions
-int dryThreshold = 300;
+int dryThreshold = 430;
 
 void setup() {
   // Set up the relay pin as an output
@@ -15,8 +13,8 @@ void loop() {
   // Read the value from the moisture sensor
   sensorValue = analogRead(moistureSensor);
 
-  // Map the sensor value to a percentage (assuming 550 is 100% and 0 is 0%)
-  int moisturePercent = map(sensorValue, 0, 550, 0, 100);
+  // Map the sensor value to a percentage (assuming 600 is 100% and 0 is 0%)
+  int moisturePercent = map(sensorValue, 0, 600, 0, 0);
 
   // If the sensor value is below the dryness threshold, activate the pump
   if (sensorValue < dryThreshold) {
